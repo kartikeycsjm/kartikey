699669ead5f9cb2f690da4f430fc1ea3c79843e9
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-02-26 00:59:29 +0530

    hello

--- app/skills/skill.jsx
+++ app/skills/skill.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 const skill = ({name,img,text}) => {
   return (
-    <div className='md:flex-col md:w-[300px] md:h-[300px] border border-blue-700 my-1 min-h-[150px] w-[100%] flex items-center justify-around rounded-lg bg-blue-600'>
-        <Image src={img} className='w-[30%] md:h-[130px] md:w-[130px]'></Image>
-        <div className='w-[60%] md:min-h-[20px] md:w-[100%] text-center flex flex-col justify-around items-center text-white'>
-            <h3 className='text-[20px] font-bold underline p-1'>{name}</h3>
-            <p className='md:px-5 p-2'>{text}</p>
+    <div className='md:flex-col md:w-[300px] md:h-[300px] my-1 min-h-[150px] w-[100%] flex items-center justify-around rounded-lg bg-[#333333]'>
+        <Image src={img} className='w-[20%] md:h-[130px] md:w-[130px]'></Image>
+        <div className='w-[60%] md:min-h-[20px] md:w-[100%] text-center flex flex-col justify-around items-center text-[#cccccc]'>
+            <h3 className='text-[16px] font-bold p-1 text-[#ffffff] md:text-[19px]'>{name}</h3>
+            <p className='md:px-5 p-2 text-[13px] md:text[16px]'>{text}</p>
         </div>
     </div>
   )
