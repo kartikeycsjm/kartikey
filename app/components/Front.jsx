7ecd5acb72c6ee2b3b187fe9dd110eb3f01acf28
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-02-26 18:09:50 +0530

    hello

--- app/components/Front.jsx
+++ app/components/Front.jsx
@@ -12,35 +12,38 @@ const Front = () => {
         window.open(fullUrl, '_blank');
     };
     return (
-        <div className='w-full min-h-screen text-white flex justify-center items-center flex-col'>
-            <div id="first" className='h-screen w-full md:w-[90%] flex justify-around items-center flex-col  md:flex-row-reverse sm:flex-row-reverse'>
+        <div className='w-full text-white flex justify-center items-center flex-col'>
+            <div id="first" className='h-[650px] w-full md:w-[90%] flex justify-around items-center flex-col  md:flex-row-reverse sm:flex-row-reverse'>
                 <Image src={img} className='w-[200px] md:w-[300px] sm:w-[250px] rounded-xl'></Image>
-                <div id="text" className='flex justify-between flex-col h-[350px] md:h-[400px] text-[35px] md:w-[50%] md:text-[50px] sm:text-[45px] text-center md:text-left sm:text-left'>
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            typewriter.typeString(`Hey there! I'm <div id='name'> Kartikey Mishra </div>`)
-                                .callFunction(() => {
-                                    console.log('String typed out!');
-                                })
-                                .pauseFor(1000)
-                                .start();
-                            typewriter.typeString(`A React Developer`)
-                                .callFunction(() => {
-                                    console.log('String typed out!');
-                                })
-                                .pauseFor(1000)
-                                .start()
-                                .deleteChars(18)
-                            typewriter.typeString(`A NextJS Developer`)
-                                .callFunction(() => {
-                                    console.log('String typed out!');
-                                })
-                                .pauseFor(1000)
-                                .start()
-                        }
-                        }
-                    />
-                    <button className='w-[300px] duration-[0.5s] border p-1.5 text-[20px] border-blue-700 hover:bg-blue-500 '
+                <div id="text" className='w-[70%] flex justify-between items-center flex-col h-[300px] md:h-[400px] text-[30px] md:w-[50%] md:text-[50px] sm:text-[45px] text-center md:text-left sm:text-left'>
+                    <div className='w-full h-[150px]'>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString(`Hey there! I'm <div id='name'> Kartikey Mishra </div>`)
+                                    .callFunction(() => {
+                                        console.log('String typed out!');
+                                    })
+                                    .pauseFor(1000)
+                                    .start();
+                                typewriter.typeString(`A React Developer`)
+                                    .callFunction(() => {
+                                        console.log('String typed out!');
+                                    })
+                                    .pauseFor(1000)
+                                    .start()
+                                    .deleteChars(18)
+                                typewriter.typeString(`A NextJS Developer`)
+                                    .callFunction(() => {
+                                        console.log('String typed out!');
+                                    })
+                                    .pauseFor(1000)
+                                    .start()
+                            }
+                            }
+                        />
+                    </div>
+
+                    <button className='w-[250px] duration-[0.5s] border p-1.5 text-[20px] border-blue-700 hover:bg-blue-500 '
                         onClick={handleDownload}>
                         Download Resume
                     </button>
@@ -49,8 +52,8 @@ const Front = () => {
             </div>
             <hr />
             <div id="second" className=' h-[450px] md: w-[90%] md:w-[60%] flex items-center justify-center flex-col'>
-                <h1 className='text-[40px] underline'>About me</h1>
-                <p className='text-justify p-3 md:p-0 text-[18px]'>A third-year Computer Science and Engineering student on a mission to craft seamless digital experiences. Proficient in HTML, CSS, and JavaScript, I specialize in React.js and Next.js for modern, efficient web development. As I navigate the dynamic world of tech, my academic journey enriches my coding skills, focusing on data structures and algorithms. Join me as I explore the endless possibilities of technology and strive to leave a positive impact through code. Let's build the future together!</p>
+                <h1 className='md:text-[40px] sm:[30px] text-[20px] underline'>About me</h1>
+                <p className='text-justify p-3 md:p-0 text-[13px] sm:text-[16px] md:text-[18px]'>A third-year Computer Science and Engineering student on a mission to craft seamless digital experiences. Proficient in HTML, CSS, and JavaScript, I specialize in React.js and Next.js for modern, efficient web development. As I navigate the dynamic world of tech, my academic journey enriches my coding skills, focusing on data structures and algorithms. Join me as I explore the endless possibilities of technology and strive to leave a positive impact through code. Let's build the future together!</p>
             </div>
         </div>
     )
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,17 +4,17 @@ import img from '@/public/img.jpg'
 import Link from 'next/link'
 const Header = () => {
     return (
-        <div className='w-[97%] h-[70px] md:h-[90px] sm:h-[80px] rounded-full flex items-center 
+        <div className='w-[97%] h-[70px] md:h-[80px] sm:h-[80px] rounded-full flex items-center 
         justify-between md:justify-between bg-[rgba(0,0,0,0.95)] text-[rgb(255,255,255)]'>
             <div className='w-[40%] sm:w-[45%] md:w-[30%] h-full flex justify-between items-center'>
-                <Image src={img} className='w-[55px] md:w-[70px] rounded-full'></Image>
+                <Image src={img} className='w-[55px] md:w-[65px] rounded-full'></Image>
                 <h1 id='n' className=' cursor-pointer text-[22px] sm:text-[30px] md:text-[35px] text-[#e0e0fc]'>&lt;Kartikey/&gt;</h1>
             </div>
             <div className='w-[60%] sm:w-[55%] md:w-[70%] text-[13px] md:text-[16px] sm:text-[14px] h-full flex justify-around md:justify-around items-center'>
                 <Link className='bg-black hover:bg-blue-400 px-2 py-1 rounded-2xl' href={'/'}>Home</Link>
                 <Link className='bg-black hover:bg-blue-400 px-2 py-1 rounded-2xl' href={'/project'}>Projects</Link>
                 <Link className='bg-black hover:bg-blue-400 px-2 py-1 rounded-2xl' href={'/skills'}>Skills</Link>
-                <Link href={'/contact'} className='bg-black hover:bg-blue-400 px-2 py-1 border border-blue-600 rounded-2xl'>Contact Me</Link>
+                <Link href={'/contact'} className='bg-black hover:bg-blue-400 px-1 py-1 border border-blue-600 rounded-2xl'>Contact Me</Link>
             </div>
         </div>
     )
