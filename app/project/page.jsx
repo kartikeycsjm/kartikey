31f33f4ad5def066c0a40ab797e386b607670a73
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-02 00:14:38 +0530

    added a project

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -6,13 +6,17 @@ const page = () => {
   return (
     <div className='w-full min-h-screen flex justify-between items-center flex-col bg-[rgba(0,0,0,0.9)]'>
       <Header></Header>
-      <div id="project" className='p-5 rounded-lg w-[80%] border border-blue-500 h-[100%] flex justify-around items-center'>
+      <div id="project" className='p-5 rounded-lg w-[80%] border border-blue-500 h-[100%] flex justify-around items-center flex-col md:flex-row'>
         <Project name={'letswrite'} text={`Developed Let'sWrite, a social media platform utilizing Next.js framework with Tailwind CSS for design.
         The platform enables seamless account creation through email, username, password, and phone
         number, fostering connectivity and interaction.`}
           techstack={'Next.js, TailwindCSS, MongoDB'}
           githublink={'https://github.com/kartikeycsjm/letswrite'}
           livelink={'https://letswrite1.vercel.app'} />
+        <Project name={'Diary'} text={`Created a diary web app allowing users to add to-do lists and notes, which are saved locally for easy access.`}
+        techstack={"Next.js, Tailwind CSS"}
+          githublink={'https://github.com/kartikeycsjm/diary'}
+          livelink={'https://diary1.vercel.app'} />
       </div>
       <Footer></Footer>
     </div>
