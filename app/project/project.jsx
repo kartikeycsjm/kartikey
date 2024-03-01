72086a551c831887f18df9c6bd44c7d7dab82b6e
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-01 15:26:09 +0530

    updated resume

--- app/project/project.jsx
+++ app/project/project.jsx
@@ -5,7 +5,7 @@ const project = ({ name, text, techstack, githublink, livelink }) => {
     <div className='rounded min-h-[250px] w-[80%] bg-blue-200 flex justify-between items-center flex-col '>
       <h1 className='text-[25px] text-white bg-blue-500 m-2 px-2 py-1 rounded-md'>{name}</h1>
       <p className='px-5 py-3 flex justify-center items-center text-justify text-[14px]'>{text}</p>
-      <div id="techstack" className='text-bold'>
+      <div id="techstack" className='text-bold text-center'>
         <b>Tech Stack-</b> {techstack}
       </div>
       <div id="links" className='w-[150px] flex justify-around items-center h-[50px]'>
