86b0ab6b82e71e15df340b643daa66c4750529cf
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-01 15:33:07 +0530

    project rounded

--- app/project/project.jsx
+++ app/project/project.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 const project = ({ name, text, techstack, githublink, livelink }) => {
   return (
-    <div className='rounded min-h-[250px] w-[80%] bg-blue-200 flex justify-between items-center flex-col '>
-      <h1 className='text-[25px] text-white bg-blue-500 m-2 px-2 py-1 rounded-md'>{name}</h1>
+    <div className='rounded-lg min-h-[250px] w-[80%] md:w-[400px] bg-[#333333] text-[#cccccc] flex justify-between items-center flex-col '>
+      <h1 className='text-[20px] text-[white] bg-blue-500 m-2 px-1 py-1 rounded-md'>{name}</h1>
       <p className='px-5 py-3 flex justify-center items-center text-justify text-[14px]'>{text}</p>
       <div id="techstack" className='text-bold text-center'>
         <b>Tech Stack-</b> {techstack}
