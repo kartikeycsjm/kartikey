225ecbeaae836b64559595b3ac24abb741c35057
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-01 15:39:25 +0530

    project border

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -6,13 +6,14 @@ const page = () => {
   return (
     <div className='w-full min-h-screen flex justify-between items-center flex-col bg-[rgba(0,0,0,0.9)]'>
       <Header></Header>
-      <Project name={'letswrite'} text={`Developed Let'sWrite, a social media platform utilizing Next.js framework with Tailwind CSS for design.
+      <div id="project" className='p-5 rounded-lg w-[80%] border border-blue-500 h-[100%] flex justify-around items-center'>
+        <Project name={'letswrite'} text={`Developed Let'sWrite, a social media platform utilizing Next.js framework with Tailwind CSS for design.
         The platform enables seamless account creation through email, username, password, and phone
         number, fostering connectivity and interaction.`}
-        techstack={'Next.js, TailwindCSS, MongoDB'}
-        githublink={'https://github.com/kartikeycsjm/letswrite'}
-        livelink={'https://letswrite1.vercel.app'} />
-        
+          techstack={'Next.js, TailwindCSS, MongoDB'}
+          githublink={'https://github.com/kartikeycsjm/letswrite'}
+          livelink={'https://letswrite1.vercel.app'} />
+      </div>
       <Footer></Footer>
     </div>
   )
--- app/project/project.jsx
+++ app/project/project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 const project = ({ name, text, techstack, githublink, livelink }) => {
   return (
-    <div className='rounded-lg min-h-[250px] w-[80%] md:w-[400px] bg-[#333333] text-[#cccccc] flex justify-between items-center flex-col '>
+    <div className='rounded-lg min-h-[250px] w-[95%] md:w-[400px] bg-[#333333] text-[#cccccc] flex justify-between items-center flex-col '>
       <h1 className='text-[20px] text-[white] bg-blue-500 m-2 px-1 py-1 rounded-md'>{name}</h1>
       <p className='px-5 py-3 flex justify-center items-center text-justify text-[14px]'>{text}</p>
       <div id="techstack" className='text-bold text-center'>
